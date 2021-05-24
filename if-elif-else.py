720b1d389fb7f24f316257409a6df084b50fa02f
aparanji-123 <84369524+aparanji-123@users.noreply.github.com>
2021-05-24 15:58:40 +0530

    Create if-elif-else.py

---
+++ if-elif-else.py
@@ -0,0 +1,8 @@
+#program to demonstrate if-elif-else conditional statement
+choice=input(f'which game do you like:press\nc-cricket\nh-hockey:')
+if choice=='c':
+     print('you are a cricketer') 
+elif choice=='h':
+     print('you are a hockey player') 
+else:
+     print('you are not interested in sports') 
