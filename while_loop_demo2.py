5259f9cd1b79cef256ea067744877b839d66cd57
aparanji-123 <84369524+aparanji-123@users.noreply.github.com>
2021-05-24 16:09:24 +0530

    Create while_loop_demo2.py

---
+++ while_loop_demo2.py
@@ -0,0 +1,7 @@
+#program to print a string as many as times we want
+count=int(input('How many times you want to say 'PYTHON':'))
+i=1
+whilei<=count:
+    print('PYTHON') 
+    i+=1
+print('Thank you') 
