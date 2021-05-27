a2dd91414eea3da49930edf26de017578bc8712c
aparanji-123 <84369524+aparanji-123@users.noreply.github.com>
2021-05-27 12:35:18 +0530

    Create if_with_list.py

---
+++ if_with_list.py
@@ -0,0 +1,16 @@
+a=10
+b=20
+list =[1,2,3,4,5]
+if (a in list):
+     print('line:1-a is available in the given list')
+else
+     print('line:1-a is not available in the given list') 
+if (b not in list) 
+     print('line:2-b is not available in the given list')
+else 
+     print('line:2-b is available in the given list') 
+c=a\b
+if (c in list) 
+     print('line:3-c is available in the given list') 
+else
+     print('line:3-c is not available in the given list') 
