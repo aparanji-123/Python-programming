33c2547e6204a121f923f8ac1ef6b406123a9221
aparanji-123 <84369524+aparanji-123@users.noreply.github.com>
2021-05-27 12:00:39 +0530

    Create for_with_range.py

---
+++ for_with_range.py
@@ -0,0 +1,8 @@
+print('sum of given numbers: ') 
+Lower=int(input('Enter the number: ')) 
+Upper=int(input('Enter the number: '))
+the sum=0
+for number in range(lower, upper+1):
+the sum=the sum+number
+    print(the sum)
+ 
