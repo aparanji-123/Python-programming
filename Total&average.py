75b430ea450b773dc79b6e9fd973984d871800bb
aparanji-123 <84369524+aparanji-123@users.noreply.github.com>
2021-06-09 17:10:43 +0530

    Create Total&average.py

---
+++ Total&average.py
@@ -0,0 +1,7 @@
+a=int(input('enter a value: ')) 
+b=int(input('enter b value: ')) 
+c=int(input('enter c value: ')) 
+Total=a+b+c
+Average=(a+b+c)/3
+print('TOTAL=',Total) 
+print('AVERAGE=',Average) 
